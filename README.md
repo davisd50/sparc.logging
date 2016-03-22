9b96fe57f444d3ae5a1405e84f811f826ac26379
David Davis <davisd50@gmail.com>
2016-03-22 15:17:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+sparc.logging
+===========
---
+++ sparc/logging/__init__.py
@@ -0,0 +1,18 @@
+"""
+Module usage:
+
+from sparc.logging import logging
+logger = logging.getLogger(__name__)
+logger.debug('...')
+"""
+
+import logging
+import sys
+
+rootlogger = logging.getLogger()
+if rootlogger.level == logging.NOTSET:
+    rootlogger.setLevel(logging.WARN)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s %(name)s %(message)s'))
+rootlogger.addHandler(handler)
